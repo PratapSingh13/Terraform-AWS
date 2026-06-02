69f0b539329bdb631dbe290c6a28aa379a5ce769
Yogendra Pratap Singh <yogendrapratapsingh70@gmail.com>
2026-06-02 12:58:22 +0530

    backend updated with new bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "mybucket"
+    bucket  = "nagarro-terraform-state-file"
     key     = "terraform/state"
     region  = "ap-south-1"
     encrypt = true
